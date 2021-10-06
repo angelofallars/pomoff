52fe82c2c9b1147990fd4b08c7dadab70a2d52fa
Angelo Fallaria <ba.fallaria@gmail.com>
2021-10-06 18:08:26 +0800

    Add getch.py

---
+++ getch.py
@@ -0,0 +1,21 @@
+"""Instant input with keyboard"""
+import sys
+import termios
+import tty
+import os
+import time
+
+ 
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+ 
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+ 
+
+button_delay = 0.2
