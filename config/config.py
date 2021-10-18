f42ffbf2a7dd1a77529d69d053ed0debedce388f
Angelo Fallaria <ba.fallaria@gmail.com>
2021-10-19 01:00:28 +0800

    Add config file

---
+++ config/config.py
@@ -0,0 +1,23 @@
+# Durations (in minutes) for each Pomodoro timer interval
+# default - work_time= 25
+# default - short_break_time = 5
+# default - long_break_time = 15
+work_time = 25
+short_break_time = 5
+long_break_time = 15
+
+# Number of work intervals for the session
+# default - session_intervals = 4
+session_intervals = 4
+
+# The sound that plays when an interval ends
+# in the /assets/audio directory
+# default - "bike_ding.wav"
+# set to None to disable
+end_sound = "bike_ding.wav"
+
+# The icon for notifications
+# in the /assets directory
+# default - "tomato-256.png" 
+# set to None to disable
+notify_icon = "tomato-256.png"
